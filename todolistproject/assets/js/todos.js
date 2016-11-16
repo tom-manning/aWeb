15a92d77699f8395461c53c54a98731ed24d289c
tom-manning <tmanning20@gmail.com>
2016-11-15 18:35:20 -0800

    fixed input issue

--- todolistproject/assets/js/todos.js
+++ todolistproject/assets/js/todos.js
@@ -3,7 +3,7 @@ $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-// Click on X to delete Todo
+// Click on trashcan to delete a to-do
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
@@ -11,15 +11,26 @@ $("ul").on("click", "span", function(event){
 	event.stopPropagation();
 });
 
+//When "enter" is pressed check value of input box 
 $("input[type='text']").keypress(function(event){
 		if(event.which === 13){
+			if(this.value === ""){
+				return false
+			} else {
 			var todoText = $(this).val();
 			$(this).val("");
-			//create a new li and add to ul
-			$("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>")
+			//create a new li and add to ul (at the top of the to-do list)
+			$("ul").prepend("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>")
 		}
+	  }
 	});
 
-$(".fa-plus").click(function(){
+//Show "Add New Todo" when clicked and toggle up/down arrow
+
+$(".fa-arrow-up").click(function(){
 	$("input[type='text']").fadeToggle();
+	$(this).toggleClass('fa-arrow-down fa-arrow-up');
 });
+
+
+
